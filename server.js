19ab2526f2f5aaf926bda67b2e55b867026204bc
RohitasBehera <indians4german@gmail.com>
2016-10-09 23:37:36 +0530

    new edit

--- server.js
+++ server.js
@@ -7,22 +7,16 @@ app.get('/', function(req, res){
 });
 
 users = [];
-io.on('connection', function(socket){
-  console.log('A user connected');
+io.on('connection', function(socket){  
   socket.on('setUsername', function(data){
-    console.log(data);
-    if(users.indexOf(data) > -1){
-      socket.emit('userExists', data + ' username is taken! Try some other username.');
-    }
-    else{
+      console.log(data);     
       users.push(data);
-      socket.emit('userSet', {username: data});
-    }
+      socket.emit('userSet', {username: data});    
   });
   socket.on('msg', function(data){
       //Send message to everyone
       io.sockets.emit('newmsg', data);
-  })
+  })   
 });
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on localhost:3000');
