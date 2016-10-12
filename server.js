02af623513b4c06a15b06fc219e883da35fee2d1
RohitasBehera <indians4german@gmail.com>
2016-10-12 12:50:33 +0530

    css

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-var app = require('express')();
+var express = require("express");
+var app = express();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path')
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+//serve static files 
+app.use(express.static(__dirname + '/public'));
 
 users = [];
 io.on('connection', function(socket){  
   socket.on('setUsername', function(data){
       console.log(data);     
       users.push(data);
-      socket.emit('userSet', {username: data});    
+      socket.emit('userSet', {username: data});   	 
   });
   socket.on('msg', function(data){
       //Send message to everyone
